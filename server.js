5596bb5a893a7d8311944491c19f48fff39f8987
Administrator <admin@example.com>
2018-08-31 17:21:42 +0530

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/carbazarweb1'));
-
+app.use(cors());
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/carbazarweb1/index.html'));
