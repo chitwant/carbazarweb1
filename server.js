dd24f0e74a56f7574cd72ab1ac2f26a6eebb0c3f
chitwant <guri.preet@gmail.com>
2018-09-03 14:25:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var cors = require('cors');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(cors());
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
 app.use(express.static(__dirname + '/dist/car'));
 
 app.get('/*', function(req,res) {
