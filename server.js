f378f348a0b32c66b538b394f307026a2e7513db
chitwant <guri.preet@gmail.com>
2018-09-03 14:31:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
  app.use(cors({origin: 'https://apicar.herokuapp.com'}));
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Origin", "https://apicar.herokuapp.com");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
  });
